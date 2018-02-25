2c2c230631048d8d93bbcf8c402c83f01c341c54
Tobiplayer3 <tobikrause3@gmail.com>
2018-02-25 14:32:46 +0100

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
 void setup() {
-    // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  if(Serial.available() > 0){
+    Serial.read();
+  }
 }
