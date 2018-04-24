f5ee18eca4e20a07b985e54d4990f5e440a409e4
Tobiplayer3 <tobikrause3@gmail.com>
2018-04-24 20:03:35 +0200

    Added IDs for ultrasonic. Changed and added methods.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Stepper.h>
 
+bool on = false;
 int steps = 100;
 
 int motorR[] = {0,2,1,3};
@@ -18,6 +19,14 @@ int ultraSonicF[] = {8,9};
 
 bool automatic = true;
 
+// declare methods to access them
+float getDistance(int id);
+int getHighestDistance();
+void drive(int steps);
+void rotate(int degree);
+int* getUltraSonicByID(int id);
+bool isDistanceTooLow(int id);
+
 void setup() {
   Serial.begin(9600);
 }
@@ -26,7 +35,20 @@ void loop() {
   if(Serial.available() > 0){
     Serial.read();
 
-    if(automatic){
+    if(automatic && on){
+      if(isDistanceTooLow(0)){
+
+      }else if(isDistanceTooLow(1)){
+
+      }else if(isDistanceTooLow(2)){
+        if(getDistance(0) >= getDistance(1)){
+          rotate(90);
+        }else{
+          rotate(-90);
+        }
+      }
+
+      drive(steps);
 
     }else{
 
@@ -44,14 +66,14 @@ void drive(int steps){
 }
 
 // rotate in degree
-void rotate(int direction, int degree){
+void rotate(int degree){
 
 }
 
 int getHighestDistance(){
-  float distanceR = getDistance(ultraSonicR);
-  float distanceL = getDistance(ultraSonicL);
-  float distanceF = getDistance(ultraSonicF);
+  float distanceR = getDistance(0);
+  float distanceL = getDistance(1);
+  float distanceF = getDistance(2);
 
   if(distanceR >= distanceL && distanceR >= distanceF){
     return 0;
@@ -62,12 +84,30 @@ int getHighestDistance(){
   }
 }
 
+// check if the distance between the ultrasonic and the wall is too low
+bool isDistanceTooLow(int id){
+  float distance = getDistance(id);
+  if(distance < 5){
+    return true;
+  }
+  return false;
+}
+
 // get distance from ultrasonic in cm
-float getDistance(int ultraSonic[]){
+float getDistance(int id){
+  int* ultraSonic = getUltraSonicByID(id);
   digitalWrite(ultraSonic[0], LOW);
   digitalWrite(ultraSonic[0], HIGH);
   digitalWrite(ultraSonic[0], LOW);
 
   float duration = pulseIn(ultraSonic[1], HIGH);
   return (duration/2) * 0.03432;
+}
+
+int* getUltraSonicByID(int id){
+  switch (id) {
+    case 0: return ultraSonicR;
+    case 1: return ultraSonicL;
+    case 2: return ultraSonicF;
+  }
 }
