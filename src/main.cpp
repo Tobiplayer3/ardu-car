10b03e9ac781e1e4e3b605fa8bc3d94f2d096aa3
Tobiplayer3 <tobikrause3@gmail.com>
2018-04-24 19:35:05 +0200

    Created a few methods.

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
 #include <Arduino.h>
 #include <Stepper.h>
 
-int motorR1 = 0;
-int motorR2 = 1;
-Stepper stepperR = Stepper(100, motorR1, motorR2);
+int steps = 100;
 
-int motorL1 = 2;
-int motorL2 = 3;
-Stepper stepperL = Stepper(100, motorL1, motorL2);
+int motorR[] = {0,2,1,3};
+Stepper stepperR = Stepper(steps, motorR[0], motorR[1], motorR[2], motorR[3]);
 
-int ultraSonicRIn = 4;
-int ultraSonicROut = 5;
+int motorL[] = {4,6,5,7};
+Stepper stepperL = Stepper(steps, motorL[0], motorL[1], motorL[2], motorL[3]);
 
-int ultraSonicLIn = 6;
-int ultraSonicLOut = 7;
+// ultrasonic 0
+int ultraSonicR[] = {4,5};
+// ultrasonic 1
+int ultraSonicL[] = {6,7};
+// ultrasonic 2
+int ultraSonicF[] = {8,9};
 
-int ultraSonicFIn = 8;
-int ultraSonicFOut = 9;
+bool automatic = true;
 
 void setup() {
   Serial.begin(9600);
@@ -25,14 +25,49 @@ void setup() {
 void loop() {
   if(Serial.available() > 0){
     Serial.read();
+
+    if(automatic){
+
+    }else{
+
+    }
+
   }
 }
 
-// drive a distance in cm
-void drive(int distance){
+// drive a distance in steps
+void drive(int steps){
+  for(int i = 0; i > steps; i++){
+    stepperL.step(1);
+    stepperR.step(1);
+  }
+}
+
+// rotate in degree
+void rotate(int direction, int degree){
 
 }
 
-void rotate(int direction, int amount){
+int getHighestDistance(){
+  float distanceR = getDistance(ultraSonicR);
+  float distanceL = getDistance(ultraSonicL);
+  float distanceF = getDistance(ultraSonicF);
 
+  if(distanceR >= distanceL && distanceR >= distanceF){
+    return 0;
+  }else if(distanceL >= distanceR && distanceL >= distanceF){
+    return 1;
+  }else{
+    return 2;
+  }
+}
+
+// get distance from ultrasonic in cm
+float getDistance(int ultraSonic[]){
+  digitalWrite(ultraSonic[0], LOW);
+  digitalWrite(ultraSonic[0], HIGH);
+  digitalWrite(ultraSonic[0], LOW);
+
+  float duration = pulseIn(ultraSonic[1], HIGH);
+  return (duration/2) * 0.03432;
 }
